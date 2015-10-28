94990e50b10e59ba923d026cab56d7e05bf52054
birgimad <birgitte92madsen@gmail.com>
2015-10-28 18:19:15 +0100

    adding code blah

--- cpp/BLAH/main.cpp
+++ cpp/BLAH/main.cpp
@@ -1,19 +1,81 @@
+#include <cmath>
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 using namespace std;
+#include "include\armadillo"
+using namespace arma;
 
 int main()
 {
-    int x = 0;
-    int y = 0;
+    int n=2;
+    double E = 0;
+    double T = 1.0;
+    mat spin_matrix(n,n);
+    spin_matrix(0,0) =-1;
+    spin_matrix(0,1) = -1;
+    spin_matrix(1,0) = 1;
+    spin_matrix(1,1) = 1;
+    double E_expectation = 0;
+    int mccycles = 20;
+
+    int x[n], y[n];
+    double w, DE_x, DE_y, DE;
     srand(time(NULL));
-    x = 2*((double) rand() / (RAND_MAX));
-    y = 2*((double) rand() / (RAND_MAX));
-    cout << x << endl;
-    cout << y << endl;
-    cout << time(NULL) << endl;
+
+    for (int cycles = 1; cycles <= mccycles; cycles++)
+        {
+    for (int k=0; k<n; k++)
+    {
+    for (int i=0; i<n; i++)
+    {
+    x[i] = 2*((double) rand() / (RAND_MAX));
+    y[i] = 2*((double) rand() / (RAND_MAX));
+    //cout << "x=" << setw(5) << x[i] << setw(5) << "y=" << setw(5) << y[i] << endl;
+    if (x[i]<1)
+    {
+        DE_x = 2*spin_matrix(x[i],y[i])*spin_matrix(x[i]+1,y[i]);
+    }
+    else
+    {
+        DE_x = 2*spin_matrix(x[i],y[i])*spin_matrix(x[i]-1,y[i]);
+    }
+    if (y[i]<1)
+    {
+        DE_y = 2*spin_matrix(x[i],y[i])*spin_matrix(x[i],y[i]+1);
+    }
+    else
+    {
+        DE_y = 2*spin_matrix(x[i],y[i])*spin_matrix(x[i],y[i]-1);
+    }
+    DE = DE_x+DE_y;
+    if (DE <= 0)
+    {
+        E += DE;
+    }
+    else
+    {
+        w = exp(-DE/T);
+        if (w > ((double) rand() / (RAND_MAX)))
+        {
+            E += DE;
+        }
+    }
+    spin_matrix(x[i],y[i]) = -spin_matrix(x[i],y[i]);
+    }
+    }
+
+    E_expectation += E;
+    cout << "cycle:" << setw(10) << cycles << setw(10) << "E:" << E << endl;
+}
+cout << "Expectation value:" << setw(20) << E_expectation/mccycles << endl;
+
     return 0;
 }
+
+
 
--- cpp/IsingForB/IsingForB/main.cpp
+++ cpp/IsingForB/IsingForB/main.cpp
@@ -10,34 +10,35 @@ using namespace std;
 #include "include\armadillo"
 using namespace arma;
 
-double metropolis(double T, double &E, mat &spin_matrix, int n, int &x)
+double metropolis(double T, double &E, mat &spin_matrix, int n)
 {
     // E is in units of E/J and T = 1/beta*J
     srand(time(NULL));
-
+    int x[n], y;
     double w, DE_x, DE_y, DE;
-    int  y;
     for (int i=0; i<n; i++)
     {
     for (int j=0; j<n; j++)
     {
-        x = 2*((double) rand() / (RAND_MAX));
+        x[i] = 2*((double) rand() / (RAND_MAX));
+
+        cout << "x=" << x[i] << endl;
         y = 2*((double) rand() / (RAND_MAX));
-    if (x<1)
+    if (x[i]<1)
     {
-        DE_x = 2*spin_matrix(x,y)*spin_matrix(x+1,y);
+        DE_x = 2*spin_matrix(x[i],y)*spin_matrix(x[i]+1,y);
     }
     else
     {
-        DE_x = 2*spin_matrix(x,y)*spin_matrix(x-1,y);
+        DE_x = 2*spin_matrix(x[i],y)*spin_matrix(x[i]-1,y);
     }
     if (y<1)
     {
-        DE_y = 2*spin_matrix(x,y)*spin_matrix(x,y+1);
+        DE_y = 2*spin_matrix(x[i],y)*spin_matrix(x[i],y+1);
     }
     else
     {
-        DE_y = 2*spin_matrix(x,y)*spin_matrix(x,y-1);
+        DE_y = 2*spin_matrix(x[i],y)*spin_matrix(x[i],y-1);
     }
     DE = DE_x+DE_y;
     if (DE <= 0)
@@ -68,14 +69,13 @@ int main()
     spin(0,1) = -1;
     spin(1,0) = 1;
     spin(1,1) = 1;
-    int x = 0;
     double E_expectation = 0;
     int mccycles = 10;
     for (int cycles = 1; cycles <= mccycles; cycles++)
     {
-        metropolis(T,E,spin, n,x);
+        metropolis(T,E,spin, n);
         E_expectation += E;
-        cout << "cycle:" << setw(20) << cycles << setw(20) << "x:" << setw(20) << x << endl;
+        cout << "cycle:" << setw(10) << cycles << setw(10) << "E:" << E << endl;
     }
     cout << "Expectation value:" << setw(20) << E_expectation << endl;
     return 0;
