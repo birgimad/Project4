9f0b1db503fd2cb93085b0f3403494293afa7efc
birgimad <birgitte92madsen@gmail.com>
2015-11-07 10:47:28 +0100

    Data for IsingForC_LargerN

--- cpp/IsingForC_LargerN/main.cpp
+++ cpp/IsingForC_LargerN/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 using namespace arma;
 
 int periodic(int entrance, int pm, int spins)
-//Takes care of the periodic boundary condition of latice
+//Takes care of the periodic boundary condition of lattice
 {
     return (entrance + pm + spins) % spins;
 }
@@ -46,11 +46,11 @@ int main()
     mat spin_matrix(n,n);
     spin_matrix.ones();
     initialize(n,T,spin_matrix,E,M);
-    cout << "E=" << E << "M=" <<M << endl;
     double E_expectation = 0, M_expectation = 0, E_squared = 0, M_squared = 0;
 
     int x[n*n], y[n*n];
     int microstates = pow(2,n*n)+1;
+    //Compute w before MC to use the fact that the change of energy only can take 5 different values
     double w[microstates];
     for (int i =0; i < microstates; i++){
         w[i] = 0;
@@ -62,8 +62,6 @@ int main()
     srand(time(NULL));
     for (int cycles = 1; cycles <= mccycles; cycles++)
         {
-    //for (int k=0; k<n; k++)
-    //{
     for (int i=0; i<n*n; i++)
     {
     x[i] = (n-0.000001)*((double) rand() / (RAND_MAX));  //making sure that the row and column number is either 0 or 1 (randomly chosen - almost uniform)
@@ -78,27 +76,39 @@ int main()
         M += 2*spin_matrix(x[i],y[i]);
     }
     }
-    //}
     //The computed energies are in the unit of E/J
     //The computed susectibilty is in the unit J*Chi
     //The computed specific heat is in the unit c_v / k_B
         E_expectation += E;
-        M_expectation += M;
+        M_expectation += fabs(M);
         E_squared += E*E;
         M_squared += M*M;
-        //cout << "cycle:" << setw(10) << cycles << setw(10) << "E:" << E << endl;
     }
-
-        susceptibility = 1/T * (M_squared/mccycles -(M_expectation/mccycles)*(M_expectation/mccycles));
-        specific_heat = 1/(T*T)*(E_squared/mccycles -(E_expectation/mccycles)*(E_expectation/mccycles));
-    cout << E_squared/mccycles << endl;
-        cout << "Number of MC cycles:" << setw(10) << mccycles << endl;
-    cout << "Most likely state" << endl;
-    cout << spin_matrix << endl;
+    susceptibility = 1/T * (M_squared/mccycles -(M_expectation/mccycles)*(M_expectation/mccycles));
+    specific_heat = 1/(T*T)*(E_squared/mccycles -(E_expectation/mccycles)*(E_expectation/mccycles));
+    cout << "Number of MC cycles:" << setw(10) << mccycles << endl;
+    //cout << "Most likely state" << endl;
+    //cout << spin_matrix << endl;
     cout << "Expectation value of energy:" << setw(10) << E_expectation/mccycles << endl;
     cout << "Expectation value of magnetization:" << setw(10) << M_expectation/mccycles << endl;
     cout << "Specific heat:" << setw(10) << specific_heat << endl;
     cout << "Susceptibilty:" << setw(10) << susceptibility << endl;
+
+    ofstream myfile ("IsingForC_LargerN_2.txt");
+        if (myfile.is_open())
+        {
+            myfile << "Expectation value of energy and magnetization and specific heat and susceptibility computed by generalized Ising method using Metropolis algorithm:" <<endl;
+            myfile << "n = " << n << endl;
+            myfile << "T = " << T << endl;
+            myfile << "Number of MC cycles:" << setw(10) << mccycles << endl;
+            //myfile << "Most likely state" << endl;
+            //myfile << spin_matrix << endl;
+            myfile << "Expectation value of energy:" << setw(10) << E_expectation/mccycles << endl;
+            myfile << "Expectation value of magnetization:" << setw(10) << M_expectation/mccycles << endl;
+            myfile << "Specific heat:" << setw(10) << specific_heat << endl;
+            myfile << "Susceptibilty:" << setw(10) << susceptibility << endl;
+
+        }
 
         return 0;
     }
